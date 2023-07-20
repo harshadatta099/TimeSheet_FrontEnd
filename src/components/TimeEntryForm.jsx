fa6653878effa83b5405cf718b0f5fbbfbce5e2d
harshadatta099 <harshaa@smbxl.com>
2023-07-20 15:34:02 +0530

    uptd!

--- src/components/TimeEntryForm.jsx
+++ src/components/TimeEntryForm.jsx
@@ -1,262 +1,261 @@
-import React, { useState } from "react";
-import { Form, Button, Table } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Form, Table, Button } from "react-bootstrap";
 import axios from "axios";
 
 const TimeEntryForm = () => {
-  const [projectName, setProjectName] = useState("");
-  const [activity, setActivity] = useState("");
-  const [task, setTask] = useState("");
-  const [weekdays, setWeekdays] = useState({
-    monday: "",
-    tuesday: "",
-    wednesday: "",
-    thursday: "",
-    friday: "",
+  
+  const convertToISODate = (inputDate) => {
+    const [month, day, year] = inputDate.split("/");
+    const formattedMonth = String(month).padStart(2, "0");
+    const formattedDay = String(day).padStart(2, "0");
+    return `${year}-${formattedMonth}-${formattedDay}`;
+  };
+  const currentDate = convertToISODate(new Date().toLocaleDateString("en-US"));
+
+  const currentDay = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
   });
-  const [timeEntries, setTimeEntries] = useState([]);
-  const handleInputChange = (day, value) => {
-    setWeekdays((prevState) => ({
-      ...prevState,
-      [day]: value,
-    }));
-  };
+  const [formData, setFormData] = useState({
+    id: 0,
+    projectName: "",
+    activity: "",
+    task: "",
+    hours: 0,
+    dateOnly: currentDate,
+  });
+  const [entries, setEntries] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
-  const [editIndex, setEditIndex] = useState(null);
-  const handleDelete = (index) => {
-    setTimeEntries((prevTimeEntries) => {
-      const updatedEntries = [...prevTimeEntries];
-      updatedEntries.splice(index, 1);
-      return updatedEntries;
-    });
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
   };
 
-  const handleEdit = (index) => {
+  const handleSubmit = async (event) => {
+    console.log(currentDate);
+    event.preventDefault();
+    try {
+      if (isEditing) {
+        // Edit existing entry
+        const response = await axios.put(
+          `http://localhost:5070/Edit?id=${formData.id}`,
+          formData
+        );
+        if (response.status === 200) {
+          // Refresh the entries after successful edit
+          await fetchEntries();
+          // Reset the form after editing
+          setFormData({
+            id: 0,
+            projectName: "",
+            activity: "",
+            task: "",
+            hours: 0,
+            dateOnly: currentDate,
+          });
+          setIsEditing(false);
+        } else {
+          throw new Error("Failed to edit data.");
+        }
+      } else {
+        // Create new entry
+        debugger;
+        const response = await axios.post(
+          "http://localhost:5070/save",
+          formData
+        );
+        if (response.status === 200) {
+          // Refresh the entries after successful save
+          console.log(response.data);
+          await fetchEntries();
+          // Reset the form after saving
+          setFormData({
+            id: 0,
+            projectName: "",
+            activity: "",
+            task: "",
+            hours: 0,
+            dateOnly: currentDate,
+          });
+        } else {
+          throw new Error("Failed to save data.");
+        }
+      }
+    } catch (error) {
+      console.error("Error saving data:", error);
+    }
+  };
+
+  const handleEditEntry = (entry) => {
     setIsEditing(true);
-    setEditIndex(index);
-
-    const entryToEdit = timeEntries[index];
-    setProjectName(entryToEdit.projectName);
-    setActivity(entryToEdit.activity);
-    setTask(entryToEdit.task);
-    setWeekdays({ ...entryToEdit.weekdays });
-  };
-
-  const handleSaveEdit = () => {
-    if (editIndex !== null) {
-      const updatedEntry = {
-        projectName,
-        activity,
-        task,
-        weekdays,
-      };
-      setTimeEntries((prevTimeEntries) => {
-        const updatedEntries = [...prevTimeEntries];
-        updatedEntries[editIndex] = updatedEntry;
-        return updatedEntries;
-      });
-
-      // Reset form fields and editing state
-      setIsEditing(false);
-      setEditIndex(null);
-      setProjectName("");
-      setActivity("");
-      setTask("");
-      setWeekdays({
-        monday: "",
-        tuesday: "",
-        wednesday: "",
-        thursday: "",
-        friday: "",
-      });
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (isEditing) {
-      handleSaveEdit();
-      return;
-    }
-
-    if (!projectName || !activity || !task) {
-      alert("Please fill  project name, activity and task");
-      return;
-    }
-    setTimeEntries((prevTimeEntries) => [
-      ...prevTimeEntries,
-      { projectName, activity, task, weekdays },
-    ]);
-    
-    console.log({ projectName, activity, task, weekdays });
-      // Reset form fields 
-    setProjectName("");
-    setActivity("");
-    setTask("");
-    setWeekdays({
-      monday: "",
-      tuesday: "",
-      wednesday: "",
-      thursday: "",
-      friday: "",
-    });
-
-    setIsEditing(false); // Reset editing state
-  };
-
-  const projectOptions = [
-    { value: "", label: "Select Project" },
-    { value: "Persona Nutrition", label: "Persona Nutrition" },
-    { value: "Puritains", label: "Puritains" },
-    { value: "Nestle Health Sciences", label: "Nestle Health Sciences" },
-    { value: "Market Central", label: "Market Central" },
-    { value: "Family Central", label: "Family Central" },
-    { value: "Internal POC", label: "Internal POC" },
-    { value: "External POC", label: "External POC" },
-    { value: "Marketing & Sales", label: "Marketing & Sales" },
-  ];
-
-  const activityOptions = [
-    { value: "", label: "Select Activity" },
-    { value: "Unit Testing", label: "Unit Testing" },
-    { value: "Acceptance Testing", label: "Acceptance Testing" },
-    { value: "Warranty/MC", label: "Warranty/MC" },
-    { value: "System Testing", label: "System Testing" },
-    { value: "coding/implementation", label: "coding/implementation" },
-    { value: "Design ", label: "Design" },
-    { value: "Support", label: "Support" },
-    { value: "Integration Testing", label: "Integration Testing" },
-    { value: "Requirement Development", label: "Requirement Development" },
-    { value: "Planning", label: "Planning" },
-    { value: "PTO", label: "PTO" },
-  ];
-
-  const getCurrentWeekdayDates = () => {
-    const today = new Date();
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-
-    return Array.from({ length: 5 }, (_, i) => {
-      const currentDay = new Date(today);
-      currentDay.setDate(today.getDate() - today.getDay() + i + 1);
-
-      const formattedDate = currentDay.toLocaleDateString("en-GB", {
-        day: "2-digit",
-        month: "2-digit",
-        year: "2-digit",
-      });
-      // console.log(formattedDate);
-      return days[i] + " " + formattedDate;
+    setFormData({
+      id: entry.id,
+      projectName: entry.projectName,
+      activity: entry.activity,
+      task: entry.task,
+      hours: entry.hours,
+      dateOnly: entry.dateOnly.slice(0, 10),
     });
   };
+
+  const handleDelete = async (entryId) => {
+    try {
+      const response = await axios.delete(`http://localhost:5070/${entryId}`);
+      if (response.status === 200) {
+        // Refresh the entries after successful delete
+        await fetchEntries();
+      } else {
+        throw new Error("Failed to delete data.");
+      }
+    } catch (error) {
+      console.error("Error deleting data:", error);
+    }
+  };
+
+  const fetchEntries = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5070/readdata?date=${currentDate}`
+      );
+      if (response.status === 200) {
+        setEntries(response.data);
+      } else {
+        throw new Error("Failed to fetch data.");
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEntries();
+    const intervalId = setInterval(fetchEntries, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <>
-      <Form onSubmit={handleSubmit}>
-        <Table responsive bordered>
-          <thead>
-            <tr>
-              <th>Project Name</th>
-              <th>Activity</th>
-              <th>Task</th>
-              {Object.entries(weekdays).map(([day], index) => (
-                <th key={day}>
-                  <div>{getCurrentWeekdayDates()[index]}</div>
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <Form.Select
-                  value={projectName}
-                  onChange={(e) => setProjectName(e.target.value)}
-                  style={{ width: "150px" }}
-                >
-                  {projectOptions.map((option) => (
-                    <option key={option.value} value={option.value}>
-                      {option.label}
-                    </option>
-                  ))}
-                </Form.Select>
-              </td>
-              <td>
-                <Form.Select
-                  value={activity}
-                  onChange={(e) => setActivity(e.target.value)}
-                  style={{ width: "150px" }}
-                >
-                  {activityOptions.map((option) => (
-                    <option key={option.value} value={option.value}>
-                      {option.label}
-                    </option>
-                  ))}
-                </Form.Select>
-              </td>
-              <td>
-                <Form.Control
-                  type="text"
-                  value={task}
-                  onChange={(e) => setTask(e.target.value)}
-                  style={{ width: "200px" }}
-                />
-              </td>
-              {Object.entries(weekdays).map(([day, value], index) => (
-                <td key={day}>
-                  <Form.Control
-                    type="number"
-                    value={value}
-                    onChange={(e) => handleInputChange(day, e.target.value)}
-                  />
-                </td>
-              ))}
-            </tr>
-          </tbody>
-        </Table>
-        <Button type="submit" className="m-2">
-          {isEditing ? "Save" : "Add"}
-        </Button>
-        {isEditing && (
-          <Button variant="secondary" onClick={() => setIsEditing(false)}>
-            Cancel
-          </Button>
-        )}
-      </Form>
-      {timeEntries.length > 0 && (
-        <div className="mt-3">
-          <Table responsive bordered>
+      <div>
+        <Form onSubmit={handleSubmit}>
+          <Table bordered>
             <thead>
               <tr>
                 <th>Project Name</th>
                 <th>Activity</th>
                 <th>Task</th>
-                {Object.entries(weekdays).map(([day, value], index) => (
-                  <th key={day}>
-                    <div>{getCurrentWeekdayDates()[index]}</div>
-                  </th>
-                ))}
-                <th>Edit</th>
-                <th>Delete</th>
+                <th>
+                  {currentDate} {currentDay}
+                </th>
               </tr>
             </thead>
             <tbody>
-              {timeEntries.map((entry, index) => (
-                <tr key={index}>
+              <tr>
+                <td>
+                  <Form.Control
+                    as="select"
+                    name="projectName"
+                    value={formData.projectName}
+                    onChange={handleInputChange}
+                    required
+                  >
+                    <option value="">Select Project</option>
+                    <option value="Persona Nutrition">Persona Nutrition</option>
+                    <option value="Nestle Health Sciences">
+                      Nestle Health Sciences
+                    </option>
+                    <option value="Market Central">Market Central</option>
+                    <option value="Family Central">Family Central</option>
+                    <option value="Internal POC">Internal POC</option>
+                    <option value="External POC">External POC</option>
+                    <option value="Marketing & Sales">Marketing & Sales</option>
+                  </Form.Control>
+                </td>
+                <td>
+                  <Form.Control
+                    as="select"
+                    name="activity"
+                    value={formData.activity}
+                    onChange={handleInputChange}
+                    required
+                  >
+                    <option value="">Select Activity</option>
+                    <option value="Unit Testing">Unit Testing</option>
+                    <option value="Acceptance Testing">
+                      Acceptance Testing
+                    </option>
+                    <option value="Warranty/MC">Warranty/MC</option>
+                    <option value="System Testing">System Testing</option>
+                    <option value="Coding/Implementation">
+                      Coding/Implementation
+                    </option>
+                    <option value="Design">Design</option>
+                    <option value="Support">Support</option>
+                    <option value="Integration Testing">
+                      Integration Testing
+                    </option>
+                    <option value="Requirements Development">
+                      Requirements Development
+                    </option>
+                    <option value="Planning">Planning</option>
+                    <option value="PTO">PTO</option>
+                  </Form.Control>
+                </td>
+                <td>
+                  <Form.Control
+                    type="text"
+                    name="task"
+                    value={formData.task}
+                    onChange={handleInputChange}
+                    placeholder="Enter Task"
+                    required
+                  />
+                </td>
+                <td>
+                  <Form.Control
+                    type="number"
+                    name="hours"
+                    value={formData.hours}
+                    onChange={handleInputChange}
+                    placeholder="Enter Number of Hours"
+                    required
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+
+          <Button type="submit">{isEditing ? "Save" : "Add"}</Button>
+        </Form>
+        <br />
+        {entries.length > 0 ? (
+          <Table bordered>
+            <thead>
+              <tr>
+                <th>Project Name</th>
+                <th>Activity</th>
+                <th>Task</th>
+                <th>Hours</th>
+                <th>Date</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {entries.map((entry) => (
+                <tr key={entry.id}>
                   <td>{entry.projectName}</td>
                   <td>{entry.activity}</td>
                   <td>{entry.task}</td>
-                  {Object.values(entry.weekdays).map((value, index) => (
-                    <td key={index}>{value}</td>
-                  ))}
-                  <td>
-                    <Button variant="info" onClick={() => handleEdit(index)}>
-                      Edit
-                    </Button>
-                  </td>
+                  <td>{entry.hours}</td>
+                  <td>{entry.dateOnly.slice(0, 10)}</td>
                   <td>
                     <Button
+                      variant="primary"
+                      onClick={() => handleEditEntry(entry)}
+                    >
+                      Edit
+                    </Button>{" "}
+                    <Button
                       variant="danger"
-                      onClick={() => handleDelete(index)}
+                      onClick={() => handleDelete(entry.id)}
                     >
                       Delete
                     </Button>
@@ -265,8 +264,10 @@ const TimeEntryForm = () => {
               ))}
             </tbody>
           </Table>
-        </div>
-      )}
+        ) : (
+          <p>No records found.</p>
+        )}
+      </div>
     </>
   );
 };
